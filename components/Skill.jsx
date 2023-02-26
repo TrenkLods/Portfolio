970a45d7cf73b8d1edf4f3d74b862eb6c72a66d5
TrenkLods <36474509+TrenkLods@users.noreply.github.com>
2023-02-26 19:35:57 +0300

    Update Skill.jsx

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-
+import Link from "next/link";
 const Skill = ({ data }) => {
   return (
     <>
-    <h1 className="git_container">GitHub projects</h1>
-      
+      <h1 className="git_container">GitHub projects</h1>
+
       <div className="skills-container">
         {
           /**/ data.map((data) => (
             <div className="skills-templatest" key={data.url}>
               <h2>{data.name}</h2>
-              <a href="data.git_url"> Link to github</a>
+              <Link href="data.git_url">
+                <h2> Link to github</h2>
+              </Link>
               <h6>Visibility : {data.visibility}</h6>
               <h6>watchers:{data.watchers}</h6>
               <h6>Time create app : {data.created_at}</h6>
